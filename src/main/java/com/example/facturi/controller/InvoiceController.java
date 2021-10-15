7a26e672cbc7d532710106bcecfe9346bb0da618
Munteanu Victor <munteanuvictor46@gmail.com>
2021-10-15 11:35:01 +0300

    changes

--- src/main/java/com/example/facturi/controller/InvoiceController.java
+++ src/main/java/com/example/facturi/controller/InvoiceController.java
@@ -1,6 +1,7 @@
 package com.example.facturi.controller;
 
 import com.example.facturi.repository.InvoiceRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -9,8 +10,12 @@ import org.springframework.web.bind.annotation.PathVariable;
 @Controller
 public class InvoiceController {
 
+    @Autowired
+    private InvoiceRepository invoiceRepository;
+
     @GetMapping("/invoices")
     public String getInvoice(Model model) {
+        model.addAttribute("invoice", invoiceRepository.findAll());
         return "invoices";
     }
 
